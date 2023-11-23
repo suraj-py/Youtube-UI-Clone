b963c2522aeb2e9a0c05ebe61f6baa84283bc1e0
Suraj Mhatre <surajmcodes@gmail.com>
2023-11-23 14:32:55 +0530

    feat: added search bar interactivity using useState and conditional rendering

--- src/layouts/PageHeader.jsx
+++ src/layouts/PageHeader.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { Menu, Upload, Bell, User, Mic, Search } from 'lucide-react'
+import React, { useState } from 'react'
+import { Menu, Upload, Bell, User, Mic, Search, ArrowLeft } from 'lucide-react'
 import logo from '../assets/logo.png'
 
 function PageHeader() {
+
+    const [showFullSearchBar, setShowFullSearchBar] = useState(false)
+
   return (
       <div className='flex gap-10 lg:gap20 justify-between pt-1 px-4 mb-6 bg-red-200'>
           {/* logo section  */}
-          <div className='flex gap-4 items-center flex-shrink-0'>
+          <div className={`gap-4 items-center flex-shrink-0 ${showFullSearchBar ? 'hidden' : 'flex'}`}>
               <button>
                   <Menu />
               </button>
@@ -18,25 +21,36 @@ function PageHeader() {
           </div>
 
           {/* search bar section  */}
-          <form className='md:flex hidden gap-4 flex-grow justify-center items-center'>
+          <form className={`md:flex gap-4 flex-grow justify-center items-center 
+          ${showFullSearchBar ? 'flex' : 'hidden'}`}>
+              {showFullSearchBar && (
+                  <button
+                      type='button'
+                      onClick={() => setShowFullSearchBar(false)}
+                      className='flex-shrik-0'>
+                  
+                      <ArrowLeft />
+              </button>)}
               <div className='flex flex-grow max-w-[600px]'>
                   <input type='search' placeholder='Search'
                       className='rounded-l-full border border-secondary-border shadow-inner
                      shadow-secondary px-4 py-1 text-lg w-full h-10 outline-none focus:border-blue-500'
                   />
-                  <button className='py-1 px-4 rounded-r-full border-secondary-border 
+                  <button type='button'
+                      className='py-1 px-4 rounded-r-full border-secondary-border 
                             border border-l-0 flex-shrink-0 '>
                       <Search />
                   </button>
               </div>
-              <button type='button' className='flex-shrik-0'>
+              <button className='flex-shrik-0'>
                   <Mic />
               </button>
           </form>
           
           {/* other buttons  */}
-          <div className='flex flex-shrink-0  gap-6'>
-              <button className='md:hidden'>
+          <div className={`flex-shrink-0  gap-6 ${showFullSearchBar ? 'hidden' : 'flex'}`}>
+              <button onClick={() => setShowFullSearchBar(true)}
+                  className='md:hidden'>
                   <Search />
               </button>
               <button className='md:hidden'>
